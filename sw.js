ea2867278aeb10239d7ebf03def953792a64a710
Your Nameifejesu <you@example.comifejesugoodness97@gmail.com>
2020-11-25 13:01:09 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-915af2b1fd0a4a322f53.js"
+    "url": "webpack-runtime-ec0f8b6c574d4e246895.js"
   },
   {
     "url": "framework-3321703c33570853116c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d615d3b440caca9b7bb0f83817d633a1"
+    "revision": "f2b5c462c9ca4b14c08c34e4750be40b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -48,11 +48,11 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "adbcff806085dde87158ccaf2c0fd620"
+    "revision": "ddc1f5fb8f279e4a3c8f42c900cfbd4a"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "59dd6da397ac0cd0c6bf768ee60f28ce"
+    "revision": "049ebb9574b7182e53d1fe69242287ba"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
